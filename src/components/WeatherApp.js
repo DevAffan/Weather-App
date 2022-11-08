6b233e58d0385aa1ade4aaf1e2ce66e324132487
Affan Ali <affanali577@gmail.com>
2022-11-08 16:53:16 +0500

    updated

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,37 +1,59 @@
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 import "./css/style.css";
 
 const WeatherApp = ()=>{
     const [city , setcity] = useState(null);
-    const [search , setsearch] = useState(null);
+    const [search , setsearch] = useState('karachi');
     
+    useEffect( () => {
+        const fetchData = async () => {
+            const url = `https://weatherdbi.herokuapp.com/data/weather/${search}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            // console.log(city.some(cit => cit.status === "fail"));
+            setcity(data);
+        }
+        fetchData();
+    } , [search])
     return(    
         <>
-            <div className="box">
+            <div className="main">
+                <h1 className="heading">Weather App</h1>
                 <div className="inputData">
                     <input 
                         type= "search"
                         placeholder= "Search City"
                         className="inputField"
-                        onChange={()=>{
-
+                        onChange={( event )=>{
+                            setsearch(event.target.value)
                         }}
                     />
                 </div>
-                <div className="info">
-                    <h2 className="">
-                    <i className="fas fa-street-view" />{city}
-                    </h2>
-                    <h1 className="temp">
-                        5asd
-                    </h1>
-                    <h3 className="tempmin_max">
-                        min: 123 max:12312
-                    </h3>
-                </div>
+                { (city == null || city.status == 'fail') ? (<p className='errorMsg'>No Data Found</p> ): 
+                    (<div>
+                    <div className="detail" >
+                        <h1 className="temp">
+                        {city.currentConditions.temp.c} °C
+                        </h1>
+                        <h3 className="data icon-comment">
+                        <img src={city.currentConditions.iconURL} alt="Icon" /> {city.currentConditions.comment}
+                        </h3>
+                        <h2 className="location">
+                        <i class="fa fa-map-marker" />{search}
+                        </h2>
+                        <h3 className="data">
+                            {city.region} - ({city.currentConditions.dayhour})
+                        </h3>
+                        <h3 className="data">
+                        Humidity: {city.currentConditions.humidity}
+                        </h3>
+                    </div>
                 <div className="wave -one"></div>
                 <div className="wave -two"></div>
                 <div className="wave -three"></div>
+                    </div>)
+}
+                <p className="creator">Created by : Affan Ali</p>
             </div>
         </>
     )
